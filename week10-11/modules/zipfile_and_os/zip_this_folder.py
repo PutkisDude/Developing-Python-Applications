e864c3806d85088d2dcf4efcbbbb5f55d10c0b08
PutkisDude <putkis@gmail.com>
2021-04-06 12:14:53 +0300

    fixed bugs

--- week10-11/modules/zipfile_and_os/zip_this_folder.py
+++ week10-11/modules/zipfile_and_os/zip_this_folder.py
@@ -1,24 +1,46 @@
-# importing required modules
+#Author Lauri Putkonen
+#Choose 3 different modules and explain 2 functions (purpose and code) from
+#each module:
+
+#ZipFile
+# You can pack files easily
+# With this script you can pack current folder
+# which is handy if you want send multiple files somewhere
+
+# I use .write() method in this code
+# You can use read(), extract(), extractall() functions
+# and multiple others with ZipFile
+# ZipFile works quite much same as normal files, you need open it with specific mode
+
+# OS -module
+# You can use operating system dependent functionalies
+# Example in this code i get path for current file name so i don't pack it too
+# With os.path methods
+# I tried os.getcwd() to get current working directory to pack folder
+#But it packed all sub folders aswell so i decided use just "./"
+# I haven't try code on windows so not sure if it works there
+
+# Part of this code is copied from geeksforgeeks.org
+
 from zipfile import ZipFile
 import os
 import sys
 
-zip_name = "text.zip"
+zip_file = "text.zip"
 
 this_file = file_name =  os.path.basename(sys.argv[0])
-print(this_file)
   
 def get_all_file_paths(directory):
   
-    # initializing empty file paths list
     files_in_directory = []
-  
-    # crawling through directory and subdirectories
+
+        # Add file paths to array
     for root, directories, files in os.walk(directory):
         for filename in files:
-            # join the two strings in order to form the full filepath.
-            if filename == zip_name or filename == this_file:
+            # Ignore script and zip file
+            if filename == zip_file or filename == this_file: 
                 continue
+            # join the two strings in order to form the full filepath.
             filepath = os.path.join(root, filename)
             print(filepath)
             files_in_directory.append(filepath)
@@ -27,19 +49,17 @@ def get_all_file_paths(directory):
     return files_in_directory        
   
 def main():
-    # path to folder which needs to be zipped
-    directory = './.'
+
+    directory = './' # Folder to zip 
   
     # calling function to get all file paths in the directory
     file_paths = get_all_file_paths(directory)
   
-    # writing files to a zipfile
-#    with ZipFile(file,'w') as zip:
-#        # writing each file one by one
-#        for file in file_paths:
-#            zip.write(file)
+    with ZipFile(zip_file,'w') as zip:
+        for file in file_paths:
+            zip.write(file)
   
-    print('All files zipped successfully!')        
+    print('Files zipped!')        
   
   
 if __name__ == "__main__":
